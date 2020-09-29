814811e905da55335cc6eee06ae7a00d2f6dba61
ChinChoy <colin.chinchoy@fujitsu.com>
2020-09-29 09:48:52 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+"""Project name :  Variable training
+    File name : main.py
+    Programmer : Colin B. Chin Choy
+"""
+SongName = "Begin Again"
+ReleaseDate = "1/10/2012"
+Label = "Big Machine"
+Artist = "Taylor Swift"
+Genre = "Country"
+DurationInSeconds = 240
+SongWriter = "Taylor Swift"
+
+# Added the text to be printed with the info for context
+
+print ("the Song name is :- ", SongName)
+print ("The Song's Release Date :- ", ReleaseDate)
+print ("The artist is :- ", Artist)
+print ("The artist is with the Label :- ", Label)
+print ("The song falls within the gnere:- ", Genre)
+print ("The song is this length in seconds:- ", DurationInSeconds)
+print ("The song was written by :- ", SongWriter)
+
